f7fd0a9ea4b1d1d4985ced15d761835771779250
princehabeeb <habiy3400@gmail.com>
2023-09-29 10:42:57 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+
+// var text2 = 'This is interesting';
+function alertBox(){
+    // Instructions
+    // var text = 'I am working';
+
+    alert(' I am working');
+    
+}
+
+
+// arithmetic operators
+
+// +-*/% < > <= >= -= += *= /= %=
+
+
+const alertMyName = () => {
+  alert( 8 > 2);
+}
+
+// alertMyName();
+
+
+
